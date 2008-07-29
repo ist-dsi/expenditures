448a18a3af9f4e4d440e693e0ebcac01f35d6baf
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2008-07-29 17:44:14 +0000

    --forece-log

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/AcquisitionProcessState.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/AcquisitionProcessState.java
@@ -1,20 +1,49 @@
 package pt.ist.expenditureTrackingSystem.domain.acquisitions;
 
-import java.util.ResourceBundle;
+import java.util.Comparator;
 
-import pt.utl.ist.fenix.tools.util.i18n.Language;
+import org.joda.time.DateTime;
 
-public enum AcquisitionProcessState {
+import pt.ist.expenditureTrackingSystem.applicationTier.Authenticate.User;
+import pt.ist.expenditureTrackingSystem.domain.DomainException;
+import pt.ist.expenditureTrackingSystem.domain.ExpenditureTrackingSystem;
+import pt.ist.expenditureTrackingSystem.domain.organization.Person;
+import pt.ist.fenixWebFramework.security.UserView;
 
-    IN_GENESIS,
-    SUBMITTED_FOR_APPROVAL,
-    APPROVED,
-    FUNDS_ALLOCATED,
-    FUNDS_ALLOCATED_TO_SERVICE_PROVIDER;
+public class AcquisitionProcessState extends AcquisitionProcessState_Base {
 
-    public String getLocalizedName() {
-	final ResourceBundle resourceBundle = ResourceBundle.getBundle("resources.EnumerationResources", Language.getLocale());
-	return resourceBundle.getString(getClass().getSimpleName() + "." + name());
+    public static final Comparator<AcquisitionProcessState> COMPARATOR_BY_WHEN = new Comparator<AcquisitionProcessState>() {
+	@Override
+	public int compare(AcquisitionProcessState o1, AcquisitionProcessState o2) {
+	    return o1.getWhenDateTime().compareTo(o2.getWhenDateTime());
+	}
+    };
+
+    protected AcquisitionProcessState() {
+	super();
+	setExpenditureTrackingSystem(ExpenditureTrackingSystem.getInstance());
     }
 
+    public AcquisitionProcessState(final AcquisitionProcess acquisitionProcess,
+	    final AcquisitionProcessStateType acquisitionProcessStateType) {
+	this();
+	final User user = UserView.getUser();
+	final Person person = user.getPerson();
+	checkArguments(acquisitionProcess, acquisitionProcessStateType, person);
+	setAcquisitionProcess(acquisitionProcess);
+	setAcquisitionProcessStateType(acquisitionProcessStateType);
+	setWho(person);
+	setWhenDateTime(new DateTime());
+    }
+
+    private void checkArguments(AcquisitionProcess acquisitionProcess, AcquisitionProcessStateType acquisitionProcessStateType,
+	    Person person) {
+	if (acquisitionProcess == null || acquisitionProcessStateType == null || person == null) {
+	    throw new DomainException("error.wrong.AcquisitionProcessState.arguments");
+	}
+    }
+
+    public String getLocalizedName() {
+	return getAcquisitionProcessStateType().getLocalizedName();
+    }
 }
