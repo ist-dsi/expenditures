f51cf3a37165fbbad63d0ff96fe20f08b0608322
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2008-08-05 17:24:59 +0000

    exception handling

---
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/servlets/filters/ExceptionHandlerFilter.java
@@ -0,0 +1,51 @@
+package pt.ist.expenditureTrackingSystem.presentationTier.servlets.filters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class ExceptionHandlerFilter implements Filter {
+
+    @Override
+    public void destroy() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,
+	    ServletException {
+	System.out.println("entrei");
+	HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+	System.out.println(httpServletRequest.getRequestURI());
+	System.out.println(httpServletRequest.getRequestURL());
+	System.out.println(httpServletRequest.getParameterMap().toString());
+	try {
+	    filterChain.doFilter(request, response);
+	} catch (ServletException servletException) {
+	    StringWriter out = new StringWriter();
+	    servletException.getRootCause().printStackTrace(new PrintWriter(out));
+	    request.setAttribute("error", out.toString());
+	    httpServletRequest.getRequestDispatcher("/error.jsp").forward(request, response);
+	} catch (Exception exception) {
+	    StringWriter out = new StringWriter();
+	    exception.printStackTrace(new PrintWriter(out));
+	    request.setAttribute("error", out.toString());
+	    httpServletRequest.getRequestDispatcher("/error.jsp").forward(request, response);
+	}
+
+    }
+}
---
+++ expenditure-tracking/web/error.jsp
@@ -0,0 +1,9 @@
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
+
+<html>
+<body>
+	<bean:define id="error" name="error" />
+	<%= error %>
+</body>
+</html>
