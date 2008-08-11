947f62c496c07ccf89e08f7584f9c9db0fe4de4f
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-08-11 15:41:05 +0000

    Improved auto complete unit name provider

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/autoCompleteProvider/UnitNameAutoCompleteProvider.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/autoCompleteProvider/UnitNameAutoCompleteProvider.java
@@ -7,22 +7,31 @@ import java.util.Map;
 
 import pt.ist.expenditureTrackingSystem.domain.ExpenditureTrackingSystem;
 import pt.ist.expenditureTrackingSystem.domain.organization.Unit;
+import pt.utl.ist.fenix.tools.util.StringNormalizer;
 
 public class UnitNameAutoCompleteProvider implements AutoCompleteProvider {
 
     public Collection getSearchResults(Map<String, String> argsMap, String value, int maxCount) {
 	List<Unit> units = new ArrayList<Unit>();
+	String[] input = value.split(" ");
+	StringNormalizer.normalize(input);
+
 	for (Unit unit : ExpenditureTrackingSystem.getInstance().getUnits()) {
-	    String[] unitNameParts = unit.getName().split(" ");
-	    for (String namePart : unitNameParts) {
-		if (namePart.startsWith(value)) {
-		    units.add(unit);
-		    break;
-		}
+	    String unitName = StringNormalizer.normalize(unit.getName());
+	    if (hasMatch(input, unitName)) {
+		units.add(unit);
 	    }
 	}
-	    
 	return units;
+    }
+
+    private boolean hasMatch(String[] input, String unitNameParts) {
+	for (final String namePart : input) {
+	    if (unitNameParts.indexOf(namePart) == -1) {
+		return false;
+	    }
+	}
+	return true;
     }
 
 }
