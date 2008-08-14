f1f9709224929da38c838639f97466091ffb7373
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-08-14 22:18:00 +0000

    Improved Money output rendered, now the value always shows up with the same format (default is ##.##) and also displays the currency that is a label and can be not escaped in order to have HTML entities in the labels such as the "&euro;" entity.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/MoneyRenderer.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/MoneyRenderer.java
@@ -1,12 +1,31 @@
 package pt.ist.expenditureTrackingSystem.presentationTier.renderers;
 
+import java.text.DecimalFormat;
+import java.util.Currency;
+
 import pt.ist.expenditureTrackingSystem.domain.util.Money;
 import pt.ist.fenixWebFramework.renderers.OutputRenderer;
 import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlInlineContainer;
 import pt.ist.fenixWebFramework.renderers.components.HtmlText;
 import pt.ist.fenixWebFramework.renderers.layouts.Layout;
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
 
 public class MoneyRenderer extends OutputRenderer {
+
+    private String format;
+
+    private String currencyBundle;
+    
+    private Boolean escapeCurrencyLabel;
+    
+    public String getCurrencyBundle() {
+        return currencyBundle;
+    }
+
+    public void setCurrencyBundle(String currencyBundle) {
+        this.currencyBundle = currencyBundle;
+    }
 
     @Override
     protected Layout getLayout(Object object, Class type) {
@@ -15,11 +34,35 @@ public class MoneyRenderer extends OutputRenderer {
 	    @Override
 	    public HtmlComponent createComponent(Object object, Class type) {
 		Money money = (Money) object;
-		return new HtmlText(money.getValue().toPlainString());
+		Currency currency = money.getCurrency();
+		HtmlText currencyText = new HtmlText(RenderUtils.getResourceString(getCurrencyBundle(), "label." + currency.getCurrencyCode()),getEscapeCurrencyLabel());
+		HtmlText valueText = new HtmlText(new DecimalFormat(getFormat()).format(money.getValue()));
+		
+		HtmlInlineContainer container = new HtmlInlineContainer();
+		container.addChild(valueText);
+		container.addChild(currencyText);
+		
+		return container;
 	    }
 	    
 	};
 	
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Boolean getEscapeCurrencyLabel() {
+        return escapeCurrencyLabel;
+    }
+
+    public void setEscapeCurrencyLabel(Boolean escapeCurrencyLabel) {
+        this.escapeCurrencyLabel = escapeCurrencyLabel;
     }
 
 }
