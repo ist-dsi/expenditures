7b2a990208db0d91744eac71b2a5b49f4b22fca8
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-08-20 15:31:51 +0000

    Improved performance

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/autoCompleteProvider/CPVAutoCompleteProvider.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/autoCompleteProvider/CPVAutoCompleteProvider.java
@@ -12,16 +12,17 @@ public class CPVAutoCompleteProvider implements AutoCompleteProvider {
 
     public Collection getSearchResults(Map<String, String> argsMap, String value, int maxCount) {
 	List<CPVReference> result = new ArrayList<CPVReference>();
+	
+	String[] values = value.split(" ");
 	for (final CPVReference cpvCode : ExpenditureTrackingSystem.getInstance().getCPVReferences()) {
-	    if (cpvCode.getCode().startsWith(value) || match(cpvCode.getDescription(), value)) {
+	    if (cpvCode.getCode().startsWith(value) || match(cpvCode.getDescription(), values)) {
 		result.add(cpvCode);
 	    }
 	}
 	return result;
     }
 
-    private boolean match(String description, String input) {
-	String[] inputParts = input.split(" ");
+    private boolean match(String description, String[] inputParts) {
 	
 	for (final String namePart : inputParts) {
 	    if (description.indexOf(namePart) == -1) {
