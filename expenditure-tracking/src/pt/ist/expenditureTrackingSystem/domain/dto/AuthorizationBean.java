9b0300b529b0dd3cfba5b2700c74717465ea0538
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-08-21 15:02:30 +0000

    Missing file

---
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/dto/AuthorizationBean.java
@@ -0,0 +1,46 @@
+package pt.ist.expenditureTrackingSystem.domain.dto;
+
+import java.io.Serializable;
+
+import pt.ist.expenditureTrackingSystem.domain.authorizations.Authorization;
+import pt.ist.expenditureTrackingSystem.domain.organization.Person;
+import pt.ist.fenixWebFramework.util.DomainReference;
+
+public class AuthorizationBean implements Serializable {
+
+    DomainReference<Authorization> authorization;
+    DomainReference<Person> person;
+    Boolean canDelegate;
+    
+    public AuthorizationBean(Authorization authorization) {
+	setAuthorization(authorization);
+	setPerson(null);
+	setCanDelegate(Boolean.FALSE);
+    }
+
+    public Authorization getAuthorization() {
+        return authorization.getObject();
+    }
+
+    public void setAuthorization(Authorization authorization) {
+        this.authorization = new DomainReference<Authorization>(authorization);
+    }
+
+    public Person getPerson() {
+        return person.getObject();
+    }
+
+    public void setPerson(Person person) {
+        this.person = new DomainReference<Person>(person);
+    }
+
+    public Boolean getCanDelegate() {
+        return canDelegate;
+    }
+
+    public void setCanDelegate(Boolean caDelegate) {
+        this.canDelegate = caDelegate;
+    }
+    
+    
+}
