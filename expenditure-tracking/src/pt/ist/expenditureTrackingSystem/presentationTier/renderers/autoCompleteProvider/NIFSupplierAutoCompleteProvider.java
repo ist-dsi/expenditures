e082cea80b294f321f3ffa3b045c8c7632756fd7
Luis Cruz <luis.cruz@ist.utl.pt>
2008-09-11 19:55:00 +0000

    Search for suppliers by name as well as by there fiscal code.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/autoCompleteProvider/NIFSupplierAutoCompleteProvider.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/renderers/autoCompleteProvider/NIFSupplierAutoCompleteProvider.java
@@ -7,16 +7,35 @@ import java.util.Map;
 
 import pt.ist.expenditureTrackingSystem.domain.ExpenditureTrackingSystem;
 import pt.ist.expenditureTrackingSystem.domain.organization.Supplier;
+import pt.utl.ist.fenix.tools.util.StringNormalizer;
 
 public class NIFSupplierAutoCompleteProvider implements AutoCompleteProvider {
 
     public Collection getSearchResults(Map<String, String> argsMap, String value, int maxCount) {
-	List<Supplier> result = new ArrayList<Supplier>();
+	final List<Supplier> result = new ArrayList<Supplier>();
+	final String[] input = value.split(" ");
+	StringNormalizer.normalize(input);
+
 	for (final Supplier supplier : ExpenditureTrackingSystem.getInstance().getSuppliers()) {
 	    if (supplier.getFiscalIdentificationCode().startsWith(value)) {
 		result.add(supplier);
+	    } else {
+		final String name = StringNormalizer.normalize(supplier.getName());
+		if (hasMatch(input, name)) {
+		    result.add(supplier);
+		}
 	    }
 	}
 	return result;
     }
+
+    private boolean hasMatch(final String[] input, final String unitNameParts) {
+	for (final String namePart : input) {
+	    if (unitNameParts.indexOf(namePart) == -1) {
+		return false;
+	    }
+	}
+	return true;
+    }
+
 }
