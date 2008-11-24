12ec5af3526ae7d4cf388cee445637620c334c8c
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-11-24 12:16:23 +0000

    Modified the way mimetype is being checked. Now using MimetypesFileTypeMap

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/File.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/File.java
@@ -1,26 +1,19 @@
 package pt.ist.expenditureTrackingSystem.domain;
 
-import pt.ist.expenditureTrackingSystem.domain.util.ByteArray;
+import javax.activation.MimetypesFileTypeMap;
 
+import pt.ist.expenditureTrackingSystem.domain.util.ByteArray;
 
 public class File extends File_Base {
 
-    public static final String CONTENT_TYPE_UNKNOWN = "application/unknown";
-
-    public static final String EXTENSION_PDF = "pdf";
-    public static final String CONTENT_TYPE_PDF = "application/pdf";
-
     public File() {
-        super();
-        setOjbConcreteClass(getClass().getName());
-        setExpenditureTrackingSystem(ExpenditureTrackingSystem.getInstance());
+	super();
+	setOjbConcreteClass(getClass().getName());
+	setExpenditureTrackingSystem(ExpenditureTrackingSystem.getInstance());
     }
 
     protected String guessContentType(final String filename) {
-	if (filename.endsWith(EXTENSION_PDF)) {
-	    return CONTENT_TYPE_PDF;
-	}
-	return CONTENT_TYPE_UNKNOWN;
+	return new MimetypesFileTypeMap().getContentType(filename);
     }
 
     @Override
