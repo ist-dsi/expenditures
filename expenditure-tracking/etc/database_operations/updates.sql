a6c5ba9260f7e61f5db68fc1a67500ac2ec9690f
Luis Cruz <luis.cruz@ist.utl.pt>
2008-12-05 00:06:25 +0000

    Deployed HEAD.

--- expenditure-tracking/etc/database_operations/updates.sql
+++
@@ -1,9 +0,0 @@
-delete FROM FILE WHERE OJB_CONCRETE_CLASS='pt.ist.expenditureTrackingSystem.domain.acquisitions.afterthefact.ImportFile' AND ID_INTERNAL NOT IN (SELECT KEY_IMPORT_FILE FROM GENERIC_PROCESS WHERE KEY_IMPORT_FILE IS NOT NULL);
-
-
-
--- Inserted at 2008-12-03T16:10:33.053Z
-
-alter table FILE add column ACTIVE tinyint(1) default '1';
-
-
