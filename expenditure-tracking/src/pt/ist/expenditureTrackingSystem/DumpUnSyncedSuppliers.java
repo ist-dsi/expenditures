aaf2eeed5a8ded1f52303369bc756851d60cab52
Luis Cruz <luis.cruz@ist.utl.pt>
2008-12-15 15:43:26 +0000

    Script for dumping suppliers that are not present or matched in giaf database.

---
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/DumpUnSyncedSuppliers.java
@@ -0,0 +1,44 @@
+package pt.ist.expenditureTrackingSystem;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import pt.ist.expenditureTrackingSystem._development.PropertiesManager;
+import pt.ist.expenditureTrackingSystem.domain.ExpenditureTrackingSystem;
+import pt.ist.expenditureTrackingSystem.domain.organization.Supplier;
+import pt.ist.fenixWebFramework.FenixWebFramework;
+import pt.ist.fenixWebFramework.services.Service;
+
+public class DumpUnSyncedSuppliers {
+
+    public static void init() {
+	String domainModelPath = "web/WEB-INF/classes/domain_model.dml";
+	FenixWebFramework.initialize(PropertiesManager.getFenixFrameworkConfig(domainModelPath));
+	ExpenditureTrackingSystem.initialize(FenixWebFramework.getConfig());
+    }
+
+    public static void main(String[] args) {
+	init();
+	try {
+	    dump();
+	} catch (final IOException e) {
+	    throw new Error(e);
+	}
+	System.out.println("Done.");
+    }
+
+    @Service
+    public static void dump() throws IOException {
+	final PrintWriter printWriter = new PrintWriter("/tmp/fornecedores.csv");
+	for (final Supplier supplier : ExpenditureTrackingSystem.getInstance().getSuppliersSet()) {
+	    if (supplier.getGiafKey() == null || supplier.getGiafKey().isEmpty()) {
+		printWriter.append(supplier.getFiscalIdentificationCode());
+		printWriter.append("\t");
+		printWriter.append(supplier.getName());
+		printWriter.append("\n");
+	    }
+	}
+	printWriter.close();
+    }
+
+}
