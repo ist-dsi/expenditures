0206d55cee0827fb21d18d2bb23cbdee9b6991ca
Luis Cruz <luis.cruz@ist.utl.pt>
2009-01-13 12:48:02 +0000

    Deployed HEAD.

--- expenditure-tracking/etc/database_operations/updates.sql
+++ expenditure-tracking/etc/database_operations/updates.sql
@@ -1,4 +1,1 @@
-UPDATE GENERIC_LOG SET OPERATION="GenericAddPayingUnit" WHERE OPERATION="AddPayingUnit";     
-UPDATE GENERIC_LOG SET OPERATION="GenericRemovePayingUnit" WHERE OPERATION="RemovePayingUnit";     
-UPDATE GENERIC_LOG SET OPERATION="GenericAssignPayingUnitToItem" WHERE OPERATION="AssignPayingUnitToItem";     
-UPDATE GENERIC_LOG SET OPERATION="AuthorizeAcquisitionProcess" WHERE OPERATION="ApproveAcquisitionProcess";
+
