0754315b9e71df17afd2383457de1b160fdfa0c9
Luis Cruz <luis.cruz@ist.utl.pt>
2009-01-14 11:53:01 +0000

    Deployed HEAD.

--- expenditure-tracking/etc/database_operations/updates.sql
+++ expenditure-tracking/etc/database_operations/updates.sql
@@ -1,3 +1,1 @@
 
-UPDATE GENERIC_LOG SET OPERATION="UnApprove" WHERE OPERATION="UnApproveAcquisitionProcess";
-UPDATE GENERIC_LOG SET OPERATION="Authorize" WHERE OPERATION="AuthorizeAcquisitionProcess";
