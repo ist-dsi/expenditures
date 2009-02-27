73d13d7f795f7c035979b360b14feeca818f35d1
Luis Cruz <luis.cruz@ist.utl.pt>
2009-02-27 15:50:08 +0000

    Fixed activities availability.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/simplified/activities/FundAllocationExpirationDate.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/simplified/activities/FundAllocationExpirationDate.java
@@ -24,6 +24,7 @@ public class FundAllocationExpirationDate extends GenericAcquisitionProcessActiv
 		//&& process.getAcquisitionProcessState().isPendingFundAllocation()
 		&& process.getAcquisitionRequest().isSubmittedForFundsAllocationByAllResponsibles()
 		&& !process.isPendingFundAllocation()
+		&& !process.getAcquisitionRequest().hasAnyFundAllocationId()
 		;
     }
 
