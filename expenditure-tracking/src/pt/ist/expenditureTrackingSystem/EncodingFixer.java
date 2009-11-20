7353637ef12a6de41061844029d42800666e6124
Luis Cruz <luis.cruz@ist.utl.pt>
2009-11-20 11:57:32 +0000

    Improved implementation without using native java code.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/EncodingFixer.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/EncodingFixer.java
@@ -1,11 +1,70 @@
 package pt.ist.expenditureTrackingSystem;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.CharacterCodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import jvstm.TransactionalCommand;
+import module.fileSupport.domain.GenericFile;
+import myorg._development.PropertiesManager;
+import myorg.domain.MyOrg;
+import pt.ist.fenixWebFramework.FenixWebFramework;
+import pt.ist.fenixframework.pstm.Transaction;
+import pt.utl.ist.fenix.tools.util.i18n.Language;
 
 public class EncodingFixer {
 
-    private static String decode(final String value,final String resultEncoding)
-    		throws UnsupportedEncodingException {
+    private static void init() {
+	final String domainmodelPath = new File("build/WEB-INF/classes").getAbsolutePath();
+	System.out.println("domainmodelPath: " + domainmodelPath);
+	final File dir = new File(domainmodelPath);
+	final List<String> urls = new ArrayList<String>();
+	for (final File file : dir.listFiles()) {
+	    if (file.isFile() && file.getName().endsWith(".dml")) {
+		try {
+		    urls.add(file.getCanonicalPath());
+		} catch (IOException e) {
+		    e.printStackTrace();
+		    throw new Error(e);
+		}
+	    }
+	}
+	Collections.sort(urls);
+	final String[] paths = new String[urls.size()];
+	for (int i = 0; i < urls.size(); i++) {
+	    paths[i] = urls.get(i);
+	}
+
+	Language.setDefaultLocale(new Locale("pt", "PT"));
+	Language.setLocale(Language.getDefaultLocale());
+
+	FenixWebFramework.initialize(PropertiesManager.getFenixFrameworkConfig(paths));
+    }
+
+    public static void main(String[] args) {
+	init();
+	Transaction.withTransaction(false, new TransactionalCommand() {
+	    @Override
+	    public void doIt() {
+		try {
+		    fix();
+		} catch (final Throwable e) {
+		    throw new Error(e);
+		}
+	    }
+
+	});
+
+	System.out.println("Done.");
+    }
+
+    private static String decode(final String value, final String resultEncoding) throws UnsupportedEncodingException {
 	final byte[] bytes = value.getBytes(resultEncoding);
 	return new String(bytes);
     }
@@ -41,6 +100,23 @@ public class EncodingFixer {
 	    }
 	}
 	return true;
+    }
+
+    public static void fix() throws UnsupportedEncodingException, SecurityException, IllegalArgumentException,
+	    NoSuchMethodException, IllegalAccessException, InvocationTargetException, CharacterCodingException {
+
+	for (final GenericFile genericFile : MyOrg.getInstance().getGenericFilesSet()) {
+	    final String displayName = genericFile.getDisplayName();
+	    if (displayName != null) {
+		final String fix = fix(displayName);
+		if (fix != null && !displayName.equals(fix)) {
+		    System.out.println("Original    : " + displayName);
+		    System.out.println("Possible fix: " + fix);
+		    System.out.println();
+		    genericFile.setDisplayName(fix);
+		}
+	    }
+	}
     }
 
 }
