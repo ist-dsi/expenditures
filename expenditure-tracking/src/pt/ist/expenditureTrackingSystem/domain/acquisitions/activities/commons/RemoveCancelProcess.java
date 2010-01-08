b0b048774acf34a76cf8a29d75212c6bb39c139c
Luis Cruz <luis.cruz@ist.utl.pt>
2010-01-08 12:14:34 +0000

    Resolved compile erros. Added remove cancel activity to refund process.

---
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/activities/commons/RemoveCancelProcess.java
@@ -0,0 +1,44 @@
+package pt.ist.expenditureTrackingSystem.domain.acquisitions.activities.commons;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import module.workflow.activities.ActivityInformation;
+import module.workflow.activities.WorkflowActivity;
+import myorg.domain.User;
+import myorg.util.BundleUtil;
+import pt.ist.expenditureTrackingSystem.domain.ProcessState;
+import pt.ist.expenditureTrackingSystem.domain.RoleType;
+import pt.ist.expenditureTrackingSystem.domain.acquisitions.PaymentProcess;
+
+public class RemoveCancelProcess<P extends PaymentProcess> extends WorkflowActivity<P, ActivityInformation<P>> {
+    
+    @Override
+    public boolean isActive(P process, User user) {
+	return user != null && user.getExpenditurePerson().hasRoleType(RoleType.MANAGER) && process.isCanceled();
+    }
+
+    @Override
+    protected void process(ActivityInformation<P> activityInformation) {
+	final PaymentProcess process = activityInformation.getProcess();
+	final List<ProcessState> states = new ArrayList<ProcessState>(process.getProcessStates());
+	if (states.size() > 1) {
+	    Collections.sort(states, ProcessState.COMPARATOR_BY_WHEN);
+	    final int previous = states.size() - 2;
+	    final ProcessState processState = states.get(previous);
+	    process.revertToState(processState);
+	}
+    }
+
+    @Override
+    public String getLocalizedName() {
+	return BundleUtil.getStringFromResourceBundle(getUsedBundle(), "label." + getClass().getName());
+    }
+
+    @Override
+    public String getUsedBundle() {
+	return "resources/AcquisitionResources";
+    }
+
+}
