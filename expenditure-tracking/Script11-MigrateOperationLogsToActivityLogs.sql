b53811d1abe53b4a824ca82194eea06884020294
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-01-20 12:08:49 +0000

    Scripts were already ran in production, so we can just delete them.

--- expenditure-tracking/Script11-MigrateOperationLogsToActivityLogs.sql
+++
@@ -1,3 +0,0 @@
-select @CLASS_ID := DOMAIN_CLASS_ID FROM FF$DOMAIN_CLASS_INFO WHERE DOMAIN_CLASS_NAME='module.workflow.domain.ActivityLog';
-
-update WORKFLOW_LOG SET OJB_CONCRETE_CLASS='module.workflow.domain.ActivityLog', OID=(@CLASS_ID << 32) + ID_INTERNAL WHERE OJB_CONCRETE_CLASS='pt.ist.expenditureTrackingSystem.domain.acquisitions.OperationLog';
