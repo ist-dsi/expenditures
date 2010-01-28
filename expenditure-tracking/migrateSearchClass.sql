76a557ab7535af8ddf02f5fe8e3ff1841f87c5fa
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-01-28 17:55:21 +0000

    Script already ran in production it can be removed.

--- expenditure-tracking/migrateSearchClass.sql
+++
@@ -1,4 +0,0 @@
-alter table SAVED_SEARCH ADD COLUMN SEARCH_PROCESS_VALUES text;
-
-update SAVED_SEARCH set SEARCH_PROCESS_VALUES='ACQUISITIONS' WHERE SEARCH_CLASS_NAME='pt.ist.expenditureTrackingSystem.domain.acquisitions.simplified.SimplifiedProcedureProcess';
-update SAVED_SEARCH set SEARCH_PROCESS_VALUES='REFUND' WHERE SEARCH_CLASS_NAME='pt.ist.expenditureTrackingSystem.domain.acquisitions.refund.RefundProcess';
