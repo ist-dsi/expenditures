b4f62587b1761b0aa272e1df8e21f998ccabf377
Luis Cruz <luis.cruz@ist.utl.pt>
2010-04-19 14:53:22 +0000

    Attempt country lookup in giaf country table.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/SyncSuppliersAux.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/SyncSuppliersAux.java
@@ -17,6 +17,62 @@ import pt.ist.expenditureTrackingSystem.persistenceTier.ExternalDbQuery;
 
 public class SyncSuppliersAux {
 
+    public static class GiafCountry {
+	public String pais_cod_pai;
+	public String pais_dsg_com;
+	public String pais_dsg_abv;
+	public String pais_cod_idi;
+	public String pais_cod_moe;
+	public String pais_flg_cee;
+	public String pais_dsg_iva;
+	public String pais_pai_iva;
+	public String pais_num_dig;
+	public String pais_flg_val;
+	public String pais_num_frm;
+	public String pais_portugal;
+	public String tipo_cod_post;
+	public String num_digito_cod_post;
+	public String iso_3166;
+	public String iso_4217;
+	public String utilizador_criacao;
+	public String utilizador_alteracao;
+	public String data_criacao;
+	public String data_alteracao;
+	public String pais_dsg_ing;
+	public String pais_area_geo;
+
+	GiafCountry(final ResultSet resultSet) throws SQLException {
+	    pais_cod_pai = get(resultSet, 1);
+	    pais_dsg_com = get(resultSet, 2);
+	    pais_dsg_abv = get(resultSet, 3);
+	    pais_cod_idi = get(resultSet, 4);
+	    pais_cod_moe = get(resultSet, 5);
+	    pais_flg_cee = get(resultSet, 6);
+	    pais_dsg_iva = get(resultSet, 7);
+	    pais_pai_iva = get(resultSet, 8);
+	    pais_num_dig = get(resultSet, 9);
+	    pais_flg_val = get(resultSet, 10);
+	    pais_num_frm = get(resultSet, 11);
+	    pais_portugal = get(resultSet, 12);
+	    tipo_cod_post = get(resultSet, 13);
+	    num_digito_cod_post = get(resultSet, 14);
+	    iso_3166 = get(resultSet, 15);
+	    iso_4217 = get(resultSet, 16);
+	    utilizador_criacao = get(resultSet, 17);
+	    utilizador_alteracao = get(resultSet, 18);
+	    data_criacao = get(resultSet, 19);
+	    data_alteracao = get(resultSet, 20);
+	    pais_dsg_ing = get(resultSet, 21);
+	    pais_area_geo = get(resultSet, 22);
+	}
+
+	private String get(final ResultSet resultSet, final int i) throws SQLException {
+	    final String string = resultSet.getString(i);
+	    return string == null || string.length() == 0 ? " " : string.replace('\n', ' ').replace('\t', ' ');
+	}
+
+    }
+
     public static class GiafSupplier {
 	public String codEnt;
 	public String numFis;
@@ -76,6 +132,52 @@ public class SyncSuppliersAux {
 	public Collection<GiafSupplier> getGiafSuppliers() {
 	    return giafCodEntMap.values();
 	}
+    }
+
+    private static class CountryQuery extends ExternalDbQuery {
+
+	private final Set<GiafCountry> giafCountries;
+
+	public CountryQuery(final Set<GiafCountry> giafCountries) {
+	    this.giafCountries = giafCountries;
+	}
+
+	@Override
+	protected String getQueryString() {
+	    return "SELECT " +
+		"a.pais_cod_pai," +
+		"a.pais_dsg_com," +
+		"a.pais_dsg_abv," +
+		"a.pais_cod_idi," +
+		"a.pais_cod_moe," +
+		"a.pais_flg_cee," +
+		"a.pais_dsg_iva," +
+		"a.pais_pai_iva," +
+		"a.pais_num_dig," +
+		"a.pais_flg_val," +
+		"a.pais_num_frm," +
+		"a.pais_portugal," +
+		"a.tipo_cod_post," +
+		"a.num_digito_cod_post," +
+		"a.iso_3166," +
+		"a.iso_4217," +
+		"a.utilizador_criacao," +
+		"a.utilizador_alteracao," +
+		"a.data_criacao," +
+		"a.data_alteracao," +
+		"a.pais_dsg_ing," +
+		"a.pais_area_geo" +
+		" FROM gidpaises a";
+	}
+
+	@Override
+	protected void processResultSet(final ResultSet resultSet) throws SQLException {
+	    while (resultSet.next()) {
+		final GiafCountry giafCountry = new GiafCountry(resultSet);
+		giafCountries.add(giafCountry);
+	    }
+	}
+
     }
 
     private static class SupplierQuery extends ExternalDbQuery {
@@ -171,6 +273,27 @@ public class SyncSuppliersAux {
 	}
     }
 
+    public static class CountryReader extends ExternalDbOperation {
+
+	private final Set<GiafCountry> giafCountries;
+
+	public CountryReader(final Set<GiafCountry> giafCountries) {
+	    this.giafCountries = giafCountries;
+	}
+
+	@Override
+	protected String getDbPropertyPrefix() {
+	    return "db.giaf";
+	}
+
+	@Override
+	protected void doOperation() throws SQLException {
+	    final CountryQuery countryQuery = new CountryQuery(giafCountries);
+	    executeQuery(countryQuery);
+	}
+
+    }
+
     public static class SupplierReader extends ExternalDbOperation {
 
 	private final SupplierMap supplierMap;
@@ -199,6 +322,10 @@ public class SyncSuppliersAux {
     }
 
     public static void syncData() throws IOException, SQLException {
+	final Set<GiafCountry> giafCountries = new HashSet<GiafCountry>();
+	final CountryReader countryReader = new CountryReader(giafCountries);
+	countryReader.execute();
+
 	final SupplierMap supplierMap = new SupplierMap();
 	final SupplierReader supplierReader = new SupplierReader(supplierMap);
 	supplierReader.execute();
@@ -215,7 +342,7 @@ public class SyncSuppliersAux {
 	    }
 	    Supplier supplier = findSupplierByGiafKey(giafSupplier.codEnt);
 	    if (supplier == null && !giafSupplier.canceled && !shouldDiscard(giafSupplier)) {
-		final String country = getCountry(giafSupplier);
+		final String country = getCountry(giafCountries, giafSupplier);
 		final Address address = new Address(giafSupplier.ruaEnt, null, giafSupplier.codPos, giafSupplier.locEnt, country);
 		supplier = new Supplier(giafSupplier.nom_ent, giafSupplier.nom_ent_abv, giafSupplier.numFis, address,
 			giafSupplier.telEnt, giafSupplier.faxEnt, giafSupplier.email, null);
@@ -223,7 +350,7 @@ public class SyncSuppliersAux {
 		created++;
 	    } else if (supplier != null) {
 		matched++;
-		updateSupplierInformation(supplier, giafSupplier);
+		updateSupplierInformation(giafCountries, supplier, giafSupplier);
 	    }
 	    suppliersFromGiaf.add(supplier);
 	}
@@ -255,7 +382,7 @@ public class SyncSuppliersAux {
 	return (giafKey.length() == 6 && giafKey.charAt(0) == '1') || (giafKey.length() == 10 && giafKey.charAt(0) == 'E');
     }
 
-    private static void updateSupplierInformation(final Supplier supplier, final GiafSupplier giafSupplier) {
+    private static void updateSupplierInformation(final Set<GiafCountry> giafCountries, final Supplier supplier, final GiafSupplier giafSupplier) {
 	if (giafSupplier.canceled || shouldDiscard(giafSupplier)) {
 	    if (giafSupplier.canceled) {
 		System.out.println("Closing canceled supplier: " + supplier.getExternalId());
@@ -266,21 +393,21 @@ public class SyncSuppliersAux {
 		System.out.println("Deleting supplier " + supplier.getExternalId());
 		supplier.delete();
 	    } else {
-		updateSupplierInformationAux(supplier, giafSupplier);
+		updateSupplierInformationAux(giafCountries, supplier, giafSupplier);
 		if (!supplier.getSupplierLimit().equals(Money.ZERO)) {
 		    supplier.setSupplierLimit(Money.ZERO);
 		}
 	    }
 	} else {
-	    updateSupplierInformationAux(supplier, giafSupplier);
+	    updateSupplierInformationAux(giafCountries, supplier, giafSupplier);
 	    if (supplier.getSupplierLimit().equals(Money.ZERO)) {
 		supplier.setSupplierLimit(Supplier.SOFT_SUPPLIER_LIMIT);
 	    }
 	}
     }
 
-    private static void updateSupplierInformationAux(final Supplier supplier, final GiafSupplier giafSupplier) {
-	final String country = getCountry(giafSupplier);
+    private static void updateSupplierInformationAux(final Set<GiafCountry> giafCountries, final Supplier supplier, final GiafSupplier giafSupplier) {
+	final String country = getCountry(giafCountries, giafSupplier);
 	if (!giafSupplier.numFis.equals(supplier.getFiscalIdentificationCode())) {
 	    supplier.setFiscalIdentificationCode(giafSupplier.numFis);
 	}
@@ -315,8 +442,16 @@ public class SyncSuppliersAux {
 	return null;
     }
 
-    private static String getCountry(GiafSupplier giafSupplier) {
-	return giafSupplier.codPai != null && giafSupplier.codPai.equals("P") ? "Portugal" : "?";
+    private static String getCountry(final Set<GiafCountry> giafCountries, final GiafSupplier giafSupplier) {
+	if (giafSupplier.codPai != null) {
+	    for (final GiafCountry giafCountry : giafCountries) {
+		if (giafSupplier.codPai.equals(giafCountry.pais_cod_pai)) {
+		    return giafCountry.pais_dsg_com;
+		}
+	    }
+	}
+	return "?";
+//	return giafSupplier.codPai != null && giafSupplier.codPai.equals("P") ? "Portugal" : "?";
     }
 
 }
