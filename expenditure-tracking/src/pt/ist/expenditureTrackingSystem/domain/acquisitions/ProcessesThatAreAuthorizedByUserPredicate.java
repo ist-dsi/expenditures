4ee0d41c62a440954a2a51d9f902b8713831aef3
Luis Cruz <luis.cruz@ist.utl.pt>
2010-05-18 16:26:49 +0000

    Minor Change: This is an alternative to filter processes if someone with less autorization value has access to some process operation.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/ProcessesThatAreAuthorizedByUserPredicate.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/ProcessesThatAreAuthorizedByUserPredicate.java
@@ -2,6 +2,8 @@ package pt.ist.expenditureTrackingSystem.domain.acquisitions;
 
 import java.util.List;
 
+import myorg.domain.util.Money;
+
 import org.apache.commons.collections.Predicate;
 
 import pt.ist.expenditureTrackingSystem.domain.authorizations.Authorization;
@@ -33,6 +35,36 @@ public class ProcessesThatAreAuthorizedByUserPredicate implements Predicate {
     }
 
     private boolean evaluate(Unit unit, PaymentProcess process) {
+/*
+ * This is an alternative to filter processes if someone with less autorization value
+ * has access to some process operation.
+ * 
+	Money minForUser = null;
+	Money minForOther = null; 
+	for (final Authorization authorization : unit.getAuthorizationsSet()) {
+	    if (authorization.isValid()) {
+		final Person otherPerson = authorization.getPerson();
+		final Money maxAmount = authorization.getMaxAmount();
+		if (otherPerson == person) {
+		    if (maxAmount.isZero()) {
+			return true;
+		    }
+		    if (minForUser == null || minForUser.isGreaterThan(maxAmount)) {
+			minForUser = maxAmount;
+		    }
+		} else if (process.hasAnyAvailableActivity(otherPerson.getUser(), true)) {
+		    if (minForOther == null || minForOther.isGreaterThan(maxAmount)) {
+			minForOther = maxAmount;
+		    }		    
+		}
+	    }
+	}
+	if (minForOther == null) {
+	    return minForUser != null || (unit.hasParentUnit() && evaluate(unit.getParentUnit(), process));
+	} else {
+	    return minForUser != null && minForUser.isLessThanOrEqual(minForOther);
+	}
+*/
 	if (unit.hasAuthorizationsFor(person)) {
 	    return true;
 	} else {
