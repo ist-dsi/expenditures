46b99963a2d56462a56486a3a978c5673537c42c
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-08-30 14:09:00 +0000

    Minor Change: Changed the way the acquisitions application interface is created.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/actions/InterfaceCreationAction.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/presentationTier/actions/InterfaceCreationAction.java
@@ -9,8 +9,6 @@ import myorg.domain.RoleType;
 import myorg.domain.VirtualHost;
 import myorg.domain.contents.ActionNode;
 import myorg.domain.contents.Node;
-import myorg.domain.groups.AnonymousGroup;
-import myorg.domain.groups.AnyoneGroup;
 import myorg.domain.groups.PersistentGroup;
 import myorg.domain.groups.Role;
 import myorg.domain.groups.UnionGroup;
@@ -35,62 +33,21 @@ public class InterfaceCreationAction extends ContextBaseAction {
 	final VirtualHost virtualHost = getDomainObject(request, "virtualHostToManageId");
 	final Node node = getDomainObject(request, "parentOfNodesToManageId");
 
-	ActionNode.createActionNode(virtualHost, node, "/expendituresHome", "showAcquisitionAnnouncements",
-		"resources.ExpenditureResources", "link.sideBar.home.publicAnnouncements", AnonymousGroup.getInstance());
-
-	final Node homeNode = createNodeForPage(virtualHost, node, "resources.ExpenditureResources", "link.topBar.home", UserGroup.getInstance());
-//	ActionNode.createActionNode(virtualHost, homeNode, "/expendituresHome", "showActiveRequestsForProposal",
-//		"resources.ExpenditureResources", "link.sideBar.home.publicRequestsForProposal", Role.getRole(RoleType.MANAGER));
-	ActionNode.createActionNode(virtualHost, homeNode, "/expendituresHome", "showAcquisitionAnnouncements",
-		"resources.ExpenditureResources", "link.sideBar.home.publicAnnouncements", AnyoneGroup.getInstance());
-
-//	final Node requestsForProposalNode = createNodeForPage(virtualHost, node, "resources.ExpenditureResources",
-//		"link.topBar.requestForProposal", Role.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, requestsForProposalNode, "/requestForProposalProcess",
-//		"prepareCreateRequestForProposalProcess", "resources.ExpenditureResources",
-//		"link.sideBar.requestForProposal.create", Role.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, requestsForProposalNode, "/requestForProposalProcess",
-//		"searchRequestProposalProcess", "resources.ExpenditureResources", "link.sideBar.requestForProposal.search", Role
-//			.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, requestsForProposalNode, "/requestForProposalProcess", "showPendingRequests",
-//		"resources.ExpenditureResources", "link.sideBar.requestForProposal.pendingProcesses", Role
-//			.getRole(RoleType.MANAGER));
-//
-//	final Node announcementsnNode = createNodeForPage(virtualHost, node, "resources.ExpenditureResources",
-//		"link.topBar.announcements", Role.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "prepareCreateAnnouncement",
-//		"resources.ExpenditureResources", "link.sideBar.announcementProcess.createAnnouncement", Role
-//			.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "searchAnnouncementProcess",
-//		"resources.ExpenditureResources", "link.sideBar.announcementProcess.searchProcesses", Role
-//			.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "showMyProcesses",
-//		"resources.ExpenditureResources", "link.sideBar.announcementProcess.myProcesses", Role.getRole(RoleType.MANAGER));
-//	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "showPendingProcesses",
-//		"resources.ExpenditureResources", "link.sideBar.announcementProcess.pendingProcesses", Role
-//			.getRole(RoleType.MANAGER));
-
-	final Node aquisitionProcessNode = ActionNode.createActionNode(
-		virtualHost, node, "/dashBoard", "viewDigest", "resources.ExpenditureResources",
-		"link.topBar.acquisitionProcesses", UserGroup.getInstance());
+	final Node aquisitionProcessNode = ActionNode.createActionNode(virtualHost, node, "/search", "search",
+		"resources.ExpenditureResources", "link.topBar.acquisitionProcesses", UserGroup.getInstance());
 	ActionNode.createActionNode(virtualHost, aquisitionProcessNode, "/wizard", "newAcquisitionWizard",
 		"resources.ExpenditureResources", "link.sideBar.process.create", UserGroup.getInstance());
 	ActionNode.createActionNode(virtualHost, aquisitionProcessNode, "/search", "search", "resources.ExpenditureResources",
 		"link.sideBar.acquisitionProcess.search", UserGroup.getInstance());
-	ActionNode.createActionNode(virtualHost, aquisitionProcessNode, "/dashBoard", "viewDigest", "resources.ExpenditureResources",
-		"link.sideBar.acquisitionProcess.digest", UserGroup.getInstance());
 	ActionNode.createActionNode(virtualHost, aquisitionProcessNode, "/wizard", "afterTheFactOperationsWizard",
-		"resources.ExpenditureResources", "link.register",
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL)
-				.getSystemRole());
+		"resources.ExpenditureResources", "link.register", pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
 	ActionNode.createActionNode(virtualHost, aquisitionProcessNode, "/acquisitionProcess", "checkFundAllocations",
-		"resources.ExpenditureResources", "link.fundAllocations", 
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACCOUNTING_MANAGER)
-				.getSystemRole());
+		"resources.ExpenditureResources", "link.fundAllocations", pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACCOUNTING_MANAGER).getSystemRole());
 
-	final Node organizationNode = ActionNode.createActionNode(
-		virtualHost, node, "/expenditureTrackingOrganization", "viewLoggedPerson",
-		"resources.ExpenditureResources", "link.topBar.organization", UserGroup.getInstance());
+	final Node organizationNode = ActionNode.createActionNode(virtualHost, node, "/expenditureTrackingOrganization",
+		"viewLoggedPerson", "resources.ExpenditureResources", "link.topBar.organization", UserGroup.getInstance());
 	ActionNode.createActionNode(virtualHost, organizationNode, "/expenditureTrackingOrganization", "viewOrganization",
 		"resources.ExpenditureResources", "link.viewOrganization", UserGroup.getInstance());
 	ActionNode.createActionNode(virtualHost, organizationNode, "/expenditureTrackingOrganization", "searchUsers",
@@ -98,31 +55,26 @@ public class InterfaceCreationAction extends ContextBaseAction {
 	ActionNode.createActionNode(virtualHost, organizationNode, "/expenditureTrackingOrganization", "manageSuppliers",
 		"resources.ExpenditureOrganizationResources", "supplier.link.manage", UserGroup.getInstance());
 
-	final PersistentGroup statisticsGroup = pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.STATISTICS_VIEWER).getSystemRole();
-	final PersistentGroup acquisitionCentralManagerGroup = pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL_MANAGER).getSystemRole();
-	final UnionGroup statisticsOrAcquisitionCentralManagerGroup = UnionGroup.createUnionGroup(statisticsGroup, acquisitionCentralManagerGroup);
+	final PersistentGroup statisticsGroup = pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+		pt.ist.expenditureTrackingSystem.domain.RoleType.STATISTICS_VIEWER).getSystemRole();
+	final PersistentGroup acquisitionCentralManagerGroup = pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+		pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL_MANAGER).getSystemRole();
+	final UnionGroup statisticsOrAcquisitionCentralManagerGroup = UnionGroup.createUnionGroup(statisticsGroup,
+		acquisitionCentralManagerGroup);
 
-	final Node statisticsNode = ActionNode.createActionNode(
-		virtualHost, node, "/statistics", "showStatisticsReports",
-		"resources.ExpenditureResources", "link.topBar.statistics",
-		statisticsOrAcquisitionCentralManagerGroup);
+	final Node statisticsNode = ActionNode.createActionNode(virtualHost, node, "/statistics", "showStatisticsReports",
+		"resources.ExpenditureResources", "link.topBar.statistics", statisticsOrAcquisitionCentralManagerGroup);
 	ActionNode.createActionNode(virtualHost, statisticsNode, "/statistics", "showSimplifiedProcessStatistics",
-		"resources.StatisticsResources", "label.statistics.process.simplified",
-		statisticsGroup);
+		"resources.StatisticsResources", "label.statistics.process.simplified", statisticsGroup);
 	ActionNode.createActionNode(virtualHost, statisticsNode, "/statistics", "showRefundProcessStatistics",
-		"resources.StatisticsResources", "label.statistics.process.refund",
-		statisticsGroup);
+		"resources.StatisticsResources", "label.statistics.process.refund", statisticsGroup);
 	ActionNode.createActionNode(virtualHost, statisticsNode, "/statistics", "showStatisticsReports",
-		"resources.StatisticsResources", "label.statistics.reports",
-		statisticsOrAcquisitionCentralManagerGroup);
+		"resources.StatisticsResources", "label.statistics.reports", statisticsOrAcquisitionCentralManagerGroup);
 
-	final Node connectUnitsNode = ActionNode.createActionNode(
-		virtualHost, node, "/connectUnits", "showUnits",
-		"resources.ExpenditureOrganizationResources", "link.topBar.connectUnits",
-		Role.getRole(RoleType.MANAGER));
+	final Node connectUnitsNode = ActionNode.createActionNode(virtualHost, node, "/connectUnits", "showUnits",
+		"resources.ExpenditureOrganizationResources", "link.topBar.connectUnits", Role.getRole(RoleType.MANAGER));
 	ActionNode.createActionNode(virtualHost, connectUnitsNode, "/connectUnits", "listUnconnectedUnits",
-		"resources.ExpenditureOrganizationResources", "label.listUnconnectedUnits",
-		statisticsGroup);
+		"resources.ExpenditureOrganizationResources", "label.listUnconnectedUnits", statisticsGroup);
 
 	return forwardToMuneConfiguration(request, virtualHost, node);
     }
@@ -134,20 +86,24 @@ public class InterfaceCreationAction extends ContextBaseAction {
 	final Node node = getDomainObject(request, "parentOfNodesToManageId");
 
 	final Node announcementsnNode = createNodeForPage(virtualHost, node, "resources.ExpenditureResources",
-		"link.topBar.announcements",
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
+		"link.topBar.announcements", pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
 	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "prepareCreateAnnouncement",
 		"resources.ExpenditureResources", "link.sideBar.announcementProcess.createAnnouncement",
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
+		pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
 	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "searchAnnouncementProcess",
 		"resources.ExpenditureResources", "link.sideBar.announcementProcess.searchProcesses",
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
+		pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
 	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "showMyProcesses",
 		"resources.ExpenditureResources", "link.sideBar.announcementProcess.myProcesses",
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
+		pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
 	ActionNode.createActionNode(virtualHost, announcementsnNode, "/announcementProcess", "showPendingProcesses",
 		"resources.ExpenditureResources", "link.sideBar.announcementProcess.pendingProcesses",
-		pt.ist.expenditureTrackingSystem.domain.Role.getRole(pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
+		pt.ist.expenditureTrackingSystem.domain.Role.getRole(
+			pt.ist.expenditureTrackingSystem.domain.RoleType.ACQUISITION_CENTRAL).getSystemRole());
 
 	return forwardToMuneConfiguration(request, virtualHost, node);
     }
