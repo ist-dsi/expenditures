9812efacc9e2096d2406e3020cf1c72c5f6cc3b5
Luis Cruz <luis.cruz@ist.utl.pt>
2010-10-15 22:50:08 +0000

    Minor Change: Revert link to previous value.

--- working-capital/src/module/workingCapital/presentationTier/action/InterfaceCreationAction.java
+++ working-capital/src/module/workingCapital/presentationTier/action/InterfaceCreationAction.java
@@ -49,12 +49,12 @@ public class InterfaceCreationAction extends ContextBaseAction {
 	final VirtualHost virtualHost = getDomainObject(request, "virtualHostToManageId");
 	final Node node = getDomainObject(request, "parentOfNodesToManageId");
 
-	LinkNode.createLinkNode(virtualHost, node, "https://fenix-ashes.ist.utl.pt/fenixWiki/Qualidade/FundoDeManeio?action=AttachFile&do=get&target=Manual_FM.pdf", 
-		"resources.WorkingCapitalResources", "link.sideBar.workingCapital.help", UserGroup.getInstance());
-
-/*	LinkNode.createLinkNode(virtualHost, node, "https://fenix-ashes.ist.utl.pt/fenixWiki/Qualidade/FundoDeManeio", 
+/*	LinkNode.createLinkNode(virtualHost, node, "https://fenix-ashes.ist.utl.pt/fenixWiki/Qualidade/FundoDeManeio?action=AttachFile&do=get&target=Manual_FM.pdf", 
 		"resources.WorkingCapitalResources", "link.sideBar.workingCapital.help", UserGroup.getInstance());
 */
+	LinkNode.createLinkNode(virtualHost, node, "https://fenix-ashes.ist.utl.pt/fenixWiki/Qualidade/FundoDeManeio", 
+		"resources.WorkingCapitalResources", "link.sideBar.workingCapital.help", UserGroup.getInstance());
+
 
 	return forwardToMuneConfiguration(request, virtualHost, node);
     }
