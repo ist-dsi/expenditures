20847d87f46a5f8d26dce8293b8dec7e130b07b1
João Neves <joaoroxoneves@ist.utl.pt>
2010-12-13 13:36:09 +0000

    Bug Fix: [WorkingCapital] Removed a @ConsistencyPredicate annotation to avoid liveness problems.

--- working-capital/src/module/workingCapital/domain/WorkingCapitalTransaction.java
+++ working-capital/src/module/workingCapital/domain/WorkingCapitalTransaction.java
@@ -34,7 +34,15 @@ public class WorkingCapitalTransaction extends WorkingCapitalTransaction_Base {
 	return getBalance().isPositive();
     }
 
-    @ConsistencyPredicate
+    // This check cannot be a consistency predicate yet,
+    // because there are existing transactions that
+    // already have a balance different from the debt.
+    // This means that any change to these transactions
+    // would abort the operation, no matter what is done.
+    // This annotation can only be present when the
+    // consistency predicates are inconsistency-tolerant
+    // or all the data is corrected.
+    // @ConsistencyPredicate
     public boolean checkBalanceEqualsDebt() {
 	return getBalance().equals(getDebt());
     }
