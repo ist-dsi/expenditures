afcb1f30450a1bd4c54604abf9ea7dd94a5f7e73
Luis Cruz <luis.cruz@ist.utl.pt>
2010-12-21 12:23:24 +0000

    New Feature: Allow accounting unit to be changed.

---
+++ expenditure-tracking/src/module/mission/presentationTier/provider/ChangeAccountingUnitProvider.java
@@ -0,0 +1,21 @@
+package module.mission.presentationTier.provider;
+
+import module.mission.domain.activity.ChangeAccountingUnitActivityInformation;
+import pt.ist.fenixWebFramework.rendererExtensions.converters.DomainObjectKeyConverter;
+import pt.ist.fenixWebFramework.renderers.DataProvider;
+import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
+
+public class ChangeAccountingUnitProvider implements DataProvider {
+
+    @Override
+    public Converter getConverter() {
+	return new DomainObjectKeyConverter();
+    }
+
+    @Override
+    public Object provide(Object source, Object currentValue) {
+	ChangeAccountingUnitActivityInformation changeAccountingUnitActivityInformation = (ChangeAccountingUnitActivityInformation) source;
+	return changeAccountingUnitActivityInformation.getAccountingUnits();
+    }
+
+}
