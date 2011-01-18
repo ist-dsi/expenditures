9f82767105437bf6058615a67fa4f72ae2a02cbc
Luis Cruz <luis.cruz@ist.utl.pt>
2011-01-18 16:29:45 +0000

    Bug Fix: Only make project fund allocation activity available while allocations for pending financers are still missiong.

--- expenditure-tracking/src/module/mission/domain/MissionFinancer.java
+++ expenditure-tracking/src/module/mission/domain/MissionFinancer.java
@@ -189,7 +189,23 @@ public class MissionFinancer extends MissionFinancer_Base {
     }
 
     public boolean canAllocateProjectFunds(final Person person) {
-	return person != null && hasAccountingUnit() && getAccountingUnit().hasProjectAccountants(person.getUser().getExpenditurePerson());
+	return person != null
+		&& hasAccountingUnit()
+		&& getAccountingUnit().hasProjectAccountants(person.getUser().getExpenditurePerson())
+		&& hasPendingProjectFundAllocations();
+    }
+
+    private boolean hasPendingProjectFundAllocations() {
+	for (final MissionItemFinancer missionItemFinancer : getMissionItemFinancersSet()) {
+	    if (missionItemFinancer instanceof MissionItemProjectFinancer) {
+		final MissionItemProjectFinancer missionItemProjectFinancer = (MissionItemProjectFinancer) missionItemFinancer;
+		final String allocationId = missionItemProjectFinancer.getProjectFundAllocationId();
+		if (allocationId == null || allocationId.isEmpty()) {
+		    return true;
+		}
+	    }
+	}
+	return false;
     }
 
     public void unAllocateFunds(final Person person) {
