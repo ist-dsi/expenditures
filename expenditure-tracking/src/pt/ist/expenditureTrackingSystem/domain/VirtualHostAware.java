553f931c79995c1393e24e577417bcfe01866a5e
Luis Cruz <luis.cruz@ist.utl.pt>
2011-05-01 17:52:53 +0000

    Refactor: Moved virtual host aware infraestructure classes to base myorg project so it can be reused by any module.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/VirtualHostAware.java
+++
@@ -1,7 +0,0 @@
-package pt.ist.expenditureTrackingSystem.domain;
-
-import pt.ist.fenixframework.DomainObject;
-
-public interface VirtualHostAware extends DomainObject {
-    public boolean isConnectedToCurrentHost();
-}
