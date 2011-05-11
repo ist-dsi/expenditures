0f08ca0f2dfa4ebbeb05548437de1e2e4cd2e145
Luis Cruz <luis.cruz@ist.utl.pt>
2011-05-11 11:42:52 +0000

    Bug Fix: Ensure acquisition is always connected to expenditure tracking system.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/Acquisition.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/Acquisition.java
@@ -1,8 +1,14 @@
 package pt.ist.expenditureTrackingSystem.domain.acquisitions;
 
+import myorg.domain.VirtualHost;
 import pt.ist.expenditureTrackingSystem.domain.ExpenditureTrackingSystem;
 
 public abstract class Acquisition extends Acquisition_Base {
+
+    public Acquisition() {
+	super();
+	setExpenditureTrackingSystem(VirtualHost.getVirtualHostForThread().getExpenditureTrackingSystem());
+    }
 
     @Override
     public boolean isConnectedToCurrentHost() {
