679f8aed9297a6a60a8848c86a6305aea66d190a
Luis Cruz <luis.cruz@ist.utl.pt>
2012-01-23 23:58:38 +0000

    Refactor: Also export giaf diary number and transaction numbers.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/activities/commons/AllocateFundsPermanently.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/activities/commons/AllocateFundsPermanently.java
@@ -3,9 +3,14 @@ package pt.ist.expenditureTrackingSystem.domain.acquisitions.activities.commons;
 import module.workflow.activities.WorkflowActivity;
 import myorg.domain.User;
 import myorg.util.BundleUtil;
+import pt.ist.expenditureTrackingSystem.domain.acquisitions.AcquisitionFundAllocationDiaryAndTransactionReportRequest;
 import pt.ist.expenditureTrackingSystem.domain.acquisitions.Financer;
 import pt.ist.expenditureTrackingSystem.domain.acquisitions.PaymentProcess;
+import pt.ist.expenditureTrackingSystem.domain.acquisitions.ProjectFinancer;
+import pt.ist.expenditureTrackingSystem.domain.acquisitions.UnitItem;
 import pt.ist.expenditureTrackingSystem.domain.dto.FundAllocationBean;
+import pt.ist.expenditureTrackingSystem.domain.organization.AccountingUnit;
+import pt.ist.expenditureTrackingSystem.domain.organization.Unit;
 
 public class AllocateFundsPermanently<P extends PaymentProcess> extends
 	WorkflowActivity<P, AllocateFundsPermanentlyActivityInformation<P>> {
@@ -24,14 +29,24 @@ public class AllocateFundsPermanently<P extends PaymentProcess> extends
 	for (FundAllocationBean fundAllocationBean : activityInformation.getBeans()) {
 	    fundAllocationBean.getFinancer().addEffectiveFundAllocationId(fundAllocationBean.getEffectiveFundAllocationId());
 	}
+	PaymentProcess process = activityInformation.getProcess();
 	for (final FundAllocationBean bean : activityInformation.getBeans()) {
 	    final Financer financer = bean.getFinancer();
 	    final String diaryNumber = bean.getDiaryNumber();
 	    financer.addPaymentDiaryNumber(diaryNumber);
 	    final String transactionNumber = bean.getTransactionNumber();
-	    financer.addTransactionNumber(transactionNumber);	    
+	    financer.addTransactionNumber(transactionNumber);
+
+	    if (financer instanceof ProjectFinancer) {
+		final Unit unit = financer.getUnit();
+		final AccountingUnit accountingUnit = financer.getAccountingUnit();
+
+		for (final UnitItem unitItem : financer.getUnitItemsSet()) {
+		    new AcquisitionFundAllocationDiaryAndTransactionReportRequest(unitItem, process.getProcessNumber(),
+			    unit.getUnitNumber(), accountingUnit.getName(), diaryNumber, transactionNumber);
+		}
+	    }
 	}
-	P process = activityInformation.getProcess();
 	if (process.isInvoiceConfirmed() && process.areAllFundsPermanentlyAllocated()) {
 	    process.allocateFundsPermanently();
 	}
