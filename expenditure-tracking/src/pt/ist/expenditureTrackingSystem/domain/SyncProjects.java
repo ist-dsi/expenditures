8efd9bd734c0831fbd98fd215061475a95a54802
Luis Cruz <luis.cruz@ist.utl.pt>
2012-02-06 13:29:34 +0000

    Minor Change: Import ADIST project data.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/SyncProjects.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/SyncProjects.java
@@ -16,8 +16,8 @@ public class SyncProjects extends SyncProjects_Base {
 	    System.out.println("Done with IST");
 	    new SyncProjectsISTid().syncData();
 	    System.out.println("Done with IST-ID");
-//	    new SyncProjectsADIST().syncData();
-//	    System.out.println("Done with ADIST");
+	    new SyncProjectsADIST().syncData();
+	    System.out.println("Done with ADIST");
 	} catch (final IOException e) {
 	    throw new Error(e);
 	} catch (final SQLException e) {
