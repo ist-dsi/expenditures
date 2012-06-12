00932c81f0afa995924b1b05da1fc74e71bae77f
Luis Cruz <luis.cruz@ist.utl.pt>
2012-06-12 00:12:21 +0000

    Bug Fix: Avoid apparent bug from removing elements.

--- expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/AcquisitionInvoice.java
+++ expenditure-tracking/src/pt/ist/expenditureTrackingSystem/domain/acquisitions/AcquisitionInvoice.java
@@ -136,8 +136,7 @@ public class AcquisitionInvoice extends AcquisitionInvoice_Base {
     public void processRemoval() {
 	for (; !getFinancers().isEmpty(); getFinancers().get(0).removeAllocatedInvoices(this))
 	    ;
-	for (; !getProjectFinancers().isEmpty(); getProjectFinancers().get(0).removeAllocatedInvoices(this))
-	    ;
+	getProjectFinancers().clear();
 	for (; !getRequestItems().isEmpty(); getRequestItems().get(0).removeInvoicesFiles(this))
 	    ;
 	for (; !getUnitItems().isEmpty(); getUnitItems().get(0).removeConfirmedInvoices(this))
