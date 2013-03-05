dc4b5cd8a77660d04c725d80bc7d161db1c9fa4b
Luis Cruz <luis.cruz@ist.utl.pt>
2013-03-05 19:44:21 +0000

    Bug Fix: Revert behaviour to previus commit fixing person authorization chain verification order.

--- expenditure-tracking/src/main/java/module/mission/domain/PersonMissionAuthorization.java
+++ expenditure-tracking/src/main/java/module/mission/domain/PersonMissionAuthorization.java
@@ -49,216 +49,219 @@ import pt.ist.bennu.core.domain.VirtualHost;
  */
 public class PersonMissionAuthorization extends PersonMissionAuthorization_Base {
 
-    public static final Comparator<PersonMissionAuthorization> COMPARATOR_BY_PROCESS_NUMBER =
-            new Comparator<PersonMissionAuthorization>() {
-                @Override
-                public int compare(final PersonMissionAuthorization pma1, final PersonMissionAuthorization pma2) {
-                    final MissionProcess missionProcess1 = pma1.getMissionProcess();
-                    final MissionProcess missionProcess2 = pma2.getMissionProcess();
+    public static final Comparator<PersonMissionAuthorization> COMPARATOR_BY_PROCESS_NUMBER = new Comparator<PersonMissionAuthorization>() {
+	@Override
+	public int compare(final PersonMissionAuthorization pma1, final PersonMissionAuthorization pma2) {
+	    final MissionProcess missionProcess1 = pma1.getMissionProcess();
+	    final MissionProcess missionProcess2 = pma2.getMissionProcess();
 
-                    final MissionYear missionYear1 = missionProcess1.getMissionYear();
-                    final MissionYear missionYear2 = missionProcess2.getMissionYear();
+	    final MissionYear missionYear1 = missionProcess1.getMissionYear();
+	    final MissionYear missionYear2 = missionProcess2.getMissionYear();
 
-                    final int y = missionYear1.getYear().compareTo(missionYear2.getYear());
-                    if (y != 0) {
-                        return y;
-                    }
+	    final int y = missionYear1.getYear().compareTo(missionYear2.getYear());
+	    if (y != 0) {
+		return y;
+	    }
 
-                    final int number1 = Integer.parseInt(missionProcess1.getProcessNumber());
-                    final int number2 = Integer.parseInt(missionProcess2.getProcessNumber());
-                    final int n = number2 - number1;
+	    final int number1 = Integer.parseInt(missionProcess1.getProcessNumber());
+	    final int number2 = Integer.parseInt(missionProcess2.getProcessNumber());
+	    final int n = number2 - number1;
 
-                    return n == 0 ? pma1.getExternalId().compareTo(pma2.getExternalId()) : n;
-                }
-            };
+	    return n == 0 ? pma1.getExternalId().compareTo(pma2.getExternalId()) : n;
+	}
+    };
 
-    public static final Comparator<PersonMissionAuthorization> COMPARATOR_BY_DEPARTURE_DATE =
-            new Comparator<PersonMissionAuthorization>() {
-                @Override
-                public int compare(final PersonMissionAuthorization pma1, final PersonMissionAuthorization pma2) {
-                    final MissionProcess missionProcess1 = pma1.getMissionProcess();
-                    final MissionProcess missionProcess2 = pma2.getMissionProcess();
+    public static final Comparator<PersonMissionAuthorization> COMPARATOR_BY_DEPARTURE_DATE = new Comparator<PersonMissionAuthorization>() {
+	@Override
+	public int compare(final PersonMissionAuthorization pma1, final PersonMissionAuthorization pma2) {
+	    final MissionProcess missionProcess1 = pma1.getMissionProcess();
+	    final MissionProcess missionProcess2 = pma2.getMissionProcess();
 
-                    final Mission mission1 = missionProcess1.getMission();
-                    final Mission mission2 = missionProcess2.getMission();
+	    final Mission mission1 = missionProcess1.getMission();
+	    final Mission mission2 = missionProcess2.getMission();
 
-                    final DateTime departure1 = mission1.getDaparture();
-                    final DateTime departure2 = mission2.getDaparture();
+	    final DateTime departure1 = mission1.getDaparture();
+	    final DateTime departure2 = mission2.getDaparture();
 
-                    final int c = departure1.compareTo(departure2);
+	    final int c = departure1.compareTo(departure2);
 
-                    return c == 0 ? pma1.getExternalId().compareTo(pma2.getExternalId()) : c;
-                }
-            };
+	    return c == 0 ? pma1.getExternalId().compareTo(pma2.getExternalId()) : c;
+	}
+    };
 
     public PersonMissionAuthorization() {
-        super();
-        setMissionSystem(MissionSystem.getInstance());
+	super();
+	setMissionSystem(MissionSystem.getInstance());
     }
 
     public PersonMissionAuthorization(final Person subject, AuthorizationChain authorizationChain) {
-        this();
-        setSubject(subject);
-        final Unit unit = authorizationChain.getUnit();
-        setUnit(unit);
-        if (authorizationChain.getNext() != null) {
-            setNext(new PersonMissionAuthorization(subject, authorizationChain.getNext()));
-        }
+	this();
+	setSubject(subject);
+	final Unit unit = authorizationChain.getUnit();
+	setUnit(unit);
+	if (authorizationChain.getNext() != null) {
+	    setNext(new PersonMissionAuthorization(subject, authorizationChain.getNext()));
+	}
     }
 
     public PersonMissionAuthorization(final Mission mission, final Person subject, final AuthorizationChain authorizationChain) {
-        this(subject, authorizationChain);
-        setMission(mission);
+	this(subject, authorizationChain);
+	setMission(mission);
     }
 
     public void delete() {
-        if (hasNext()) {
-            getNext().delete();
-        }
-        removePrevious();
-        removeSubject();
-        removeUnit();
-        removeMission();
-        removeAuthority();
-        removeMissionSystem();
-        deleteDomainObject();
+	if (hasNext()) {
+	    getNext().delete();
+	}
+	removePrevious();
+	removeSubject();
+	removeUnit();
+	removeMission();
+	removeAuthority();
+	removeMissionSystem();
+	deleteDomainObject();
     }
 
     public boolean canAuthoriseParticipantActivity() {
-        final User user = UserView.getCurrentUser();
-        return user != null && canAuthoriseParticipantActivity(user.getPerson());
+	final User user = UserView.getCurrentUser();
+	return user != null && canAuthoriseParticipantActivity(user.getPerson());
     }
 
     public boolean canAuthoriseParticipantActivity(final Person person) {
-        final MissionSystem instance = MissionSystem.getInstance();
-        final Set<AccountabilityType> accountabilityTypes = instance.getAccountabilityTypesThatAuthorize();
+	return canAuthoriseThisParticipantActivity(person) || canAuthoriseNextParticipantActivity(person);
+    }
 
-        if (hasAuthority() || hasDelegatedAuthority()) {
-            return false;
-        }
-        return (getUnit().hasChildAccountabilityIncludingAncestry(accountabilityTypes, person))
-                || (hasNext() && getNext().canAuthoriseParticipantActivity(person));
+    private boolean canAuthoriseNextParticipantActivity(Person person) {
+	return hasNext() && getNext().canAuthoriseParticipantActivity(person);
+    }
+
+    private boolean canAuthoriseThisParticipantActivity(final Person person) {
+	final MissionSystem instance = MissionSystem.getInstance();
+	final Set<AccountabilityType> accountabilityTypes = instance.getAccountabilityTypesThatAuthorize();
+	return !hasAuthority() && !hasDelegatedAuthority()
+		&& getUnit().hasChildAccountabilityIncludingAncestry(accountabilityTypes, person);
     }
 
     public boolean isAvailableForAuthorization() {
-        final PersonMissionAuthorization next = getNext();
-        return !hasAuthority() && !hasDelegatedAuthority() && (next == null || next.isAvailableForAuthorization());
+	final PersonMissionAuthorization next = getNext();
+	return !hasAuthority() && !hasDelegatedAuthority() && (next == null || next.isAvailableForAuthorization());
     }
 
     public boolean canUnAuthoriseParticipantActivity() {
-        final User user = UserView.getCurrentUser();
-        return user != null && canUnAuthoriseParticipantActivity(user.getPerson());
+	final User user = UserView.getCurrentUser();
+	return user != null && canUnAuthoriseParticipantActivity(user.getPerson());
     }
 
     public boolean canUnAuthoriseParticipantActivity(final Person person) {
-        if (person == getSubject()) {
-            return false;
-        }
-        final MissionSystem instance = MissionSystem.getInstance();
-        final Set<AccountabilityType> accountabilityTypes = instance.getAccountabilityTypesThatAuthorize();
-        //final AccountabilityType accountabilityType = IstAccountabilityType.PERSONNEL_RESPONSIBLE_MISSIONS.readAccountabilityType();
-        return (hasAuthority() || hasDelegatedAuthority()) && canUnAuthorise(person, accountabilityTypes)
-                && ((!hasNext()) || (!getNext().hasAuthority() && !getNext().hasDelegatedAuthority()));
+	if (person == getSubject()) {
+	    return false;
+	}
+	final MissionSystem instance = MissionSystem.getInstance();
+	final Set<AccountabilityType> accountabilityTypes = instance.getAccountabilityTypesThatAuthorize();
+	// final AccountabilityType accountabilityType =
+	// IstAccountabilityType.PERSONNEL_RESPONSIBLE_MISSIONS.readAccountabilityType();
+	return (hasAuthority() || hasDelegatedAuthority()) && canUnAuthorise(person, accountabilityTypes)
+		&& ((!hasNext()) || (!getNext().hasAuthority() && !getNext().hasDelegatedAuthority()));
     }
 
     private boolean canUnAuthorise(final Person person, final Collection<AccountabilityType> accountabilityTypes) {
-        final Unit unitForAuthorizationCheck = hasDelegatedAuthority() && hasPrevious() ? getPrevious().getUnit() : getUnit();
-        return unitForAuthorizationCheck.hasChildAccountabilityIncludingAncestry(accountabilityTypes, person)
-                || (hasNext() && getNext().canUnAuthorise(person, accountabilityTypes));
+	final Unit unitForAuthorizationCheck = hasDelegatedAuthority() && hasPrevious() ? getPrevious().getUnit() : getUnit();
+	return unitForAuthorizationCheck.hasChildAccountabilityIncludingAncestry(accountabilityTypes, person)
+		|| (hasNext() && getNext().canUnAuthorise(person, accountabilityTypes));
     }
 
     public boolean canUnAuthoriseSomeParticipantActivity(final Person person) {
-        return canUnAuthoriseParticipantActivity(person)
-                || (hasNext() && getNext().canUnAuthoriseSomeParticipantActivity(person));
+	return canUnAuthoriseParticipantActivity(person)
+		|| (hasNext() && getNext().canUnAuthoriseSomeParticipantActivity(person));
     }
 
     @Override
     public void setAuthority(final Person authority) {
-        super.setAuthority(authority);
-        final DateTime authorizationDateTime = authority == null ? null : new DateTime();
-        setAuthorizationDateTime(authorizationDateTime);
-        if (hasNext()) {
-            getNext().setDelegatedAuthority(authority);
-        }
+	super.setAuthority(authority);
+	final DateTime authorizationDateTime = authority == null ? null : new DateTime();
+	setAuthorizationDateTime(authorizationDateTime);
+	if (hasNext()) {
+	    getNext().setDelegatedAuthority(authority);
+	}
     }
 
     private void setDelegatedAuthority(final Person authority) {
-        if (authority == null) {
-            setDelegatedAuthority((FunctionDelegation) null);
-            return;
-        }
+	if (authority == null) {
+	    setDelegatedAuthority((FunctionDelegation) null);
+	    return;
+	}
 
-        final Set<AccountabilityType> accountabilityTypes = MissionSystem.getInstance().getAccountabilityTypesThatAuthorize();
-        for (final Accountability accountability : authority.getParentAccountabilitiesSet()) {
-            if (!accountability.isActiveNow() || !accountabilityTypes.contains(accountability.getAccountabilityType())) {
-                continue;
-            }
+	final Set<AccountabilityType> accountabilityTypes = MissionSystem.getInstance().getAccountabilityTypesThatAuthorize();
+	for (final Accountability accountability : authority.getParentAccountabilitiesSet()) {
+	    if (!accountability.isActiveNow() || !accountabilityTypes.contains(accountability.getAccountabilityType())) {
+		continue;
+	    }
 
-            final FunctionDelegation functionDelegation = accountability.getFunctionDelegationDelegator();
-            if (functionDelegation != null) {
-                final Accountability parentAccountability = functionDelegation.getAccountabilityDelegator();
-                if (getUnit() == parentAccountability.getParent()) {
-                    setDelegatedAuthority(functionDelegation);
-                    return;
-                }
-            }
-        }
+	    final FunctionDelegation functionDelegation = accountability.getFunctionDelegationDelegator();
+	    if (functionDelegation != null) {
+		final Accountability parentAccountability = functionDelegation.getAccountabilityDelegator();
+		if (getUnit() == parentAccountability.getParent()) {
+		    setDelegatedAuthority(functionDelegation);
+		    return;
+		}
+	    }
+	}
     }
 
     public boolean hasAnyAuthorization() {
-        return (getAuthorizationDateTime() != null && (hasAuthority() || hasDelegatedAuthority()))
-                || (hasNext() && getNext().hasAnyAuthorization());
+	return (getAuthorizationDateTime() != null && (hasAuthority() || hasDelegatedAuthority()))
+		|| (hasNext() && getNext().hasAnyAuthorization());
     }
 
     public boolean isAuthorized() {
-        return (!hasNext() && (hasAuthority() || hasDelegatedAuthority())) || (hasNext() && getNext().isAuthorized());
+	return (!hasNext() && (hasAuthority() || hasDelegatedAuthority())) || (hasNext() && getNext().isAuthorized());
     }
 
     public boolean isPreAuthorized() {
-        return !hasNext() || !getNext().hasNext() || ((hasAuthority() || hasDelegatedAuthority()) && getNext().isPreAuthorized());
+	return !hasNext() || !getNext().hasNext() || ((hasAuthority() || hasDelegatedAuthority()) && getNext().isPreAuthorized());
     }
 
     public int getChainSize() {
-        return hasNext() ? getNext().getChainSize() + 1 : 1;
+	return hasNext() ? getNext().getChainSize() + 1 : 1;
     }
 
     public Mission getAssociatedMission() {
-        final Mission mission = getMission();
-        return mission != null || !hasPrevious() ? mission : getPrevious().getAssociatedMission();
+	final Mission mission = getMission();
+	return mission != null || !hasPrevious() ? mission : getPrevious().getAssociatedMission();
     }
 
     public void clearAuthorities() {
-        if (hasNext()) {
-            getNext().clearAuthorities();
-        }
-        removeAuthority();
+	if (hasNext()) {
+	    getNext().clearAuthorities();
+	}
+	removeAuthority();
     }
 
     public MissionProcess getMissionProcess() {
-        return hasMission() ? getMission().getMissionProcess() : (hasPrevious() ? getPrevious().getMissionProcess() : null);
+	return hasMission() ? getMission().getMissionProcess() : (hasPrevious() ? getPrevious().getMissionProcess() : null);
     }
 
     public boolean isProcessTakenByOtherUser() {
-        final MissionProcess missionProcess = getMissionProcess();
-        return missionProcess.getCurrentOwner() != null && !missionProcess.isTakenByCurrentUser();
+	final MissionProcess missionProcess = getMissionProcess();
+	return missionProcess.getCurrentOwner() != null && !missionProcess.isTakenByCurrentUser();
     }
 
     public boolean isUnitObserver(final User user) {
-        final Unit unit = getUnit();
-        final pt.ist.expenditureTrackingSystem.domain.organization.Unit expenditureUnit = unit.getExpenditureUnit();
-        if (expenditureUnit != null && expenditureUnit.isUnitObserver(user)) {
-            return true;
-        }
-        return hasNext() && getNext().isUnitObserver(user);
+	final Unit unit = getUnit();
+	final pt.ist.expenditureTrackingSystem.domain.organization.Unit expenditureUnit = unit.getExpenditureUnit();
+	if (expenditureUnit != null && expenditureUnit.isUnitObserver(user)) {
+	    return true;
+	}
+	return hasNext() && getNext().isUnitObserver(user);
     }
 
     @Override
     public boolean isConnectedToCurrentHost() {
-        return getMissionSystem() == VirtualHost.getVirtualHostForThread().getMissionSystem();
+	return getMissionSystem() == VirtualHost.getVirtualHostForThread().getMissionSystem();
     }
 
     @ConsistencyPredicate
     public boolean checkIsConnectedToList() {
-        return ((hasMission() && !hasPrevious()) || (!hasMission() && hasPrevious()));
+	return ((hasMission() && !hasPrevious()) || (!hasMission() && hasPrevious()));
     }
 }
