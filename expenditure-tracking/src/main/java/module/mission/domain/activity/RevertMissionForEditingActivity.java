638448b59d57e3751cb95eb4cf35f1fc2f9dc102
JoaoRoxoNeves <JoaoRoxoNeves@gmail.com>
2013-06-24 15:33:01 +0100

    [Missions] Bug Fix: Corrected a problem with the RevertMissionForEditingActivity which did not remove process verifications and vehicle authorizations. This activity can now only run before any commitment number is registered, and before any participant is authorized.

--- expenditure-tracking/src/main/java/module/mission/domain/activity/RevertMissionForEditingActivity.java
+++ expenditure-tracking/src/main/java/module/mission/domain/activity/RevertMissionForEditingActivity.java
@@ -1,7 +1,10 @@
 package module.mission.domain.activity;
 
 import module.mission.domain.Mission;
+import module.mission.domain.MissionChangeDescription;
+import module.mission.domain.MissionFinancer;
 import module.mission.domain.MissionProcess;
+import module.mission.domain.VehiclItem;
 import module.workflow.activities.ActivityInformation;
 import pt.ist.bennu.core.domain.User;
 import pt.ist.bennu.core.util.BundleUtil;
@@ -17,15 +20,27 @@ public class RevertMissionForEditingActivity extends
     @Override
     public boolean isActive(final MissionProcess missionProcess, final User user) {
         return super.isActive(missionProcess, user) && !missionProcess.isUnderConstruction() && !missionProcess.isCanceled()
-                && missionProcess.isRequestor(user)
-                && missionProcess.getMission().getMissionVersion().getChangesAfterArrival() == null;
+                && missionProcess.isRequestor(user) && !missionProcess.hasCommitmentNumber()
+                && !missionProcess.hasAnyAuthorizedParticipants();
     }
 
     @Override
     protected void process(final RevertMissionForEditingActivityInformation activityInformation) {
         final MissionProcess missionProcess = activityInformation.getProcess();
         final Mission mission = missionProcess.getMission();
-        mission.revertMissionForEditing(activityInformation.getDescription());
+        new MissionChangeDescription(mission, activityInformation.getDescription());
+
+        for (final MissionFinancer missionFinancer : mission.getFinancerSet()) {
+            missionFinancer.clearFundAllocations();
+            missionFinancer.setApproval(null);
+        }
+        for (VehiclItem vehicle : mission.getVehicleItems()) {
+            vehicle.setAuthorized(false);
+        }
+
+        mission.setApprovalForMissionWithNoFinancers(null);
+        mission.setIsApprovedByMissionResponsible(null);
+        mission.setIsVerified(false);
         missionProcess.setIsUnderConstruction(Boolean.TRUE);
     }
 
