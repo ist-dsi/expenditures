7c54e0324c2de360ce5649bb10c86ea399f4ea86
Luis Cruz <luis.cruz@ist.utl.pt>
2015-06-08 19:24:30 +0100

    Upgraded version of jersey client and adjusted code accordingly.

--- expenditure-tracking/src/main/java/module/mission/domain/RemoteMissionProcess.java
+++ expenditure-tracking/src/main/java/module/mission/domain/RemoteMissionProcess.java
@@ -1,20 +1,16 @@
 package module.mission.domain;
 
+import javax.ws.rs.client.ClientBuilder;
+
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
 
-import pt.ist.expenditureTrackingSystem._development.Bundle;
 import pt.ist.expenditureTrackingSystem._development.ExpenditureConfiguration;
-import pt.ist.expenditureTrackingSystem.domain.util.DomainException;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.representation.Form;
 
 public class RemoteMissionProcess extends RemoteMissionProcess_Base {
 
@@ -31,39 +27,40 @@ public class RemoteMissionProcess extends RemoteMissionProcess_Base {
     }
 
     public void connect() {
-        try {
-            final String post =
-                    new Client().resource(getRemoteMissionSystem().getConnectUrl()).queryParams(getParams()).post(String.class);
-            JsonParser parser = new JsonParser();
-            final JsonElement element = parser.parse(post);
-            final JsonObject object = (JsonObject) element;
-            setRemoteOid(object.get("externalId").getAsString());
-        } catch (final UniformInterfaceException ex) {
-            final ClientResponse response = ex.getResponse();
-            if (response != null) {
-                if (response.getStatus() == 400) {
-                    throw new DomainException(Bundle.EXPENDITURE, "bad.mission.number");
-                }
-            }
-            throw new Error(ex);
-        }
+//        try {
+        final User user = Authenticate.getUser();
+        final String post =
+                ClientBuilder.newClient().target(getRemoteMissionSystem().getConnectUrl())
+                        .queryParam("processNumber", getMissionProcess().getProcessNumber())
+                        .queryParam("externalId", getMissionProcess().getExternalId())
+                        .queryParam("hostname", CoreConfiguration.getConfiguration().applicationUrl())
+                        .queryParam("remoteProcessNumber", getProcessNumber())
+                        .queryParam("username", user == null ? null : user.getUsername())
+                        .queryParam("access_token", ExpenditureConfiguration.get().apiToken()).request().post(null, String.class);
+        JsonParser parser = new JsonParser();
+        final JsonElement element = parser.parse(post);
+        final JsonObject object = (JsonObject) element;
+        setRemoteOid(object.get("externalId").getAsString());
+//        } catch (final UniformInterfaceException ex) {
+//            final ClientResponse response = ex.getResponse();
+//            if (response != null) {
+//                if (response.getStatus() == 400) {
+//                    throw new DomainException(Bundle.EXPENDITURE, "bad.mission.number");
+//                }
+//            }
+//            throw new Error(ex);
+//        }
     }
 
     public void disconnect() {
-        final String post =
-                new Client().resource(getRemoteMissionSystem().getDisconnectUrl()).queryParams(getParams()).get(String.class);
-    }
-
-    private Form getParams() {
         final User user = Authenticate.getUser();
-        final Form params = new Form();
-        params.add("processNumber", getMissionProcess().getProcessNumber());
-        params.add("externalId", getMissionProcess().getExternalId());
-        params.add("hostname", CoreConfiguration.getConfiguration().applicationUrl());
-        params.add("remoteProcessNumber", getProcessNumber());
-        params.add("username", user == null ? null : user.getUsername());
-        params.add("access_token", ExpenditureConfiguration.get().apiToken());
-        return params;
+        ClientBuilder.newClient().target(getRemoteMissionSystem().getConnectUrl())
+                .queryParam("processNumber", getMissionProcess().getProcessNumber())
+                .queryParam("externalId", getMissionProcess().getExternalId())
+                .queryParam("hostname", CoreConfiguration.getConfiguration().applicationUrl())
+                .queryParam("remoteProcessNumber", getProcessNumber())
+                .queryParam("username", user == null ? null : user.getUsername())
+                .queryParam("access_token", ExpenditureConfiguration.get().apiToken()).request().get(String.class);
     }
 
     public void delete() {
