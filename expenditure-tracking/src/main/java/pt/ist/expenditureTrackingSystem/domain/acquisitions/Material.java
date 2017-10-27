b6aeea0ac014035d1670fa4c232dab6d7ec0f72b
Maegner <franciscommaguiar@gmail.com>
2017-10-27 14:23:33 +0100

    minnor fixes material model, measureUnits added

--- expenditure-tracking/src/main/java/pt/ist/expenditureTrackingSystem/domain/acquisitions/Material.java
+++ expenditure-tracking/src/main/java/pt/ist/expenditureTrackingSystem/domain/acquisitions/Material.java
@@ -1,28 +1,28 @@
 package pt.ist.expenditureTrackingSystem.domain.acquisitions;
 
-import java.util.Iterator;
+import pt.ist.expenditureTrackingSystem.domain.ExpenditureTrackingSystem;
 
 public class Material extends Material_Base {
     
-    public Material() {
-        super();
-    }
-    
-    public void delete() {
-        deleteDomainObject();
+    public Material(String materialSapId, String description, String measurementUnit, String type, CPVReference cpv) {
+        this.setMaterialSapId(materialSapId);
+        this.setDescription(description);
+        this.setMeasurementUnit(measurementUnit);
+        this.setType(type);
+        this.setMaterialCpv(cpv);
+
+        this.setExpenditureTrackingSystem(ExpenditureTrackingSystem.getInstance());
     }
     
     @Override
     public void setMaterialCpv(CPVReference cpv){
-        
         super.setMaterialCpv(cpv);
         
-        final Iterator<RequestItem> itemIterator =   getAcquisitionRequestItemSet().iterator();
-        
-        while (itemIterator.hasNext()) {
-            itemIterator.next().setCPVReference(cpv);
+        for(final RequestItem item : this.getAcquisitionRequestItemSet()) {
+            if(item.getMaterial() == null) {
+                item.setCPVReference(cpv);
+            }
         }
-        
     }
     
 }
